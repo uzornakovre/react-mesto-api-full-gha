b294e00c3d221c6135104553814a250ecb2e2fb2
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-22 12:17:30 +0300

    init

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy; {(new Date()).getFullYear()} Mesto Russia</p>
-    </footer>
-  )
-}
-
-export default Footer;
