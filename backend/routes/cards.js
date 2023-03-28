82cc3c595594ceed78fe51b1285c2f2729c2eaa9
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-28 14:22:00 +0300

    fix: joi

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -16,7 +16,6 @@ router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(regexUrl),
-    createdAt: Joi.date(),
   }),
 }), createCard);
 router.delete('/:cardId', celebrate({
