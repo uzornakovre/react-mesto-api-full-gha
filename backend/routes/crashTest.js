3e44a0d64c5c241bc1e19cdfa33803b35b579006
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-01 15:35:26 +0300

    fix: crash test removed

--- backend/routes/crashTest.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-router.use('/crash-test', require('./crashTest'));
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
 router.use('/users', require('./users'));
