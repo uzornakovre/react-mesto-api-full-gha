a7b3c6f8c91c39636971e4a9c4d18c2321c16fe0
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-01 17:52:53 +0300

    refactor: scss 3/10

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM          from 'react-dom/client';
 import App               from './components/App';
 import reportWebVitals   from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
